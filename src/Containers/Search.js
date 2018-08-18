863feef42107dddb4fe5ca9837fa926de66f4e5d
Li Dian <dw4h@yandex.ru>
2018-08-19 00:52:54 +0300

    added search input

---
+++ src/Containers/Search.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { changePage, clearGenres } from '../Redux/actions';
+
+class Search extends Component {
+  searchRequest(e){
+    this.props.changePage(1);
+    this.props.clearGenres();
+    let query = e.target.value;
+    if (query.length > 0) {
+      this.props.history.push(`/search=${query}`);
+    } else {
+      this.props.history.push(`/`);
+    }
+  }
+  render(){
+    return(
+      <div>
+        {this.props.searchQuery &&
+          <input className="search-input" type="search" value={this.props.searchQuery} autoFocus onChange={(e) => {this.searchRequest(e)}}/>
+        }
+        {!this.props.searchQuery &&
+          <input className="search-input" type="search" onChange={(e) => {this.searchRequest(e)}}/>
+        }
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  changePage,
+  clearGenres
+};
+
+export default connect(null, mapDispatchToProps)(Search);
