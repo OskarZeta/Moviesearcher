8eca511ea10e7ce6bf602c6249337986d821f8b6
Li Dian <dw4h@yandex.ru>
2018-09-02 22:08:03 +0300

    added page for favorite films

---
+++ src/Components/Favorites.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import MovieList from '../Components/MovieList';
+
+class Favorites extends Component {
+  render(){
+    //console.log(this.props.settings);
+    return(
+      <div className="Favorites">
+        {(this.props.favorites.length === 0 || !this.props.favorites) &&
+          <span>You don't have favorites yet</span>
+        }
+        {this.props.favorites && this.props.favorites.length > 0 && Object.keys(this.props.settings).length &&
+          <MovieList settings={this.props.settings.images} favorites={this.props.favorites} favsPage={this.props.favsPage}/>
+        }
+      </div>
+    );
+  }
+}
+
+export default Favorites;
