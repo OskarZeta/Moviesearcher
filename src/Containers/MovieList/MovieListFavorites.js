7ceb3445db5c8e4bc5c4b7bf90a158172cdad212
Li Dian <dw4h@yandex.ru>
2018-10-07 15:50:30 +0300

    added list of favorite movies

---
+++ src/Containers/MovieList/MovieListFavorites.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  loadFavorites
+} from '../../Redux/actions/change_favorites';
+
+import Movie from '../../Components/Movie/Movie';
+import Spinner from '../../Components/Spinner';
+
+class MovieListFavorites extends Component {
+  makeList() {
+    let list;
+    if (this.props.favorites.length !== 0) {
+      list = this.props.favorites;
+      return list.map((movie) => {
+        return(
+          <Movie key={movie.id}
+                 id={movie.id}
+                 name={movie.title}
+                 poster={movie.poster_path}
+                 settings={this.props.settings.images}
+                 isFav = {this.props.favorites.length > 0 ? (!!this.props.favorites.filter((favMovie) => {
+                     return favMovie.id === movie.id;}).length > 0)
+                   : false}
+          />
+        );
+      });
+    } else {
+      return(
+        <span className="App__faves-empty">You don't have any favorites yet!</span>
+      );
+    }
+  }
+  componentDidMount(){
+    if (this.props.favorites.length === 0) {
+      this.props.loadFavorites();
+    }
+  }
+  componentDidUpdate(prevProps){
+    if (this.props.favorites !== prevProps.favorites) {
+      localStorage.setItem('favorites', JSON.stringify(this.props.favorites));
+    }
+  }
+  render() {
+    return(
+      <div className="container container--movielist">
+        {/*{this.props.favorites.length === 0 && <Spinner/>}*/}
+        {/*{this.props.favorites.length !== 0 && Object.keys(this.props.settings).length !== 0 && this.makeList()}*/}
+        {Object.keys(this.props.settings).length !== 0 && this.makeList()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    settings: state.settings,
+    favorites: state.favorites
+    //loading: state.loading
+  }
+};
+const mapDispatchToProps = {
+  loadFavorites
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieListFavorites);
