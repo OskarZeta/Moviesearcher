1a41bf68213c2192fb68327d0be163cd91022b12
Li Dian <dw4h@yandex.ru>
2018-10-09 21:03:54 +0300

    Changed loading spinner

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 class Spinner extends Component {
   render(){
+    {/*<div className="lds-roller">*/}
+      {/*<div></div>*/}
+      {/*<div></div>*/}
+      {/*<div></div>*/}
+      {/*<div></div>*/}
+      {/*<div></div>*/}
+      {/*<div></div>*/}
+      {/*<div></div>*/}
+      {/*<div></div>*/}
+    {/*</div>*/}
     return(
-      <div className="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="lds-ring">
         <div></div>
         <div></div>
         <div></div>
