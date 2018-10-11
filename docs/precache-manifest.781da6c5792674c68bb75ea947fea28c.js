878b0edfea04c7ed8aa5073b2f8633e782a1514e
Li Dian <dw4h@yandex.ru>
2018-10-11 19:10:44 +0300

    re-added docs folder with updated files

---
+++ docs/precache-manifest.781da6c5792674c68bb75ea947fea28c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad4b39b2b0139931b84",
+    "url": "/Moviesearcher/static/js/runtime~main.fad4b39b.js"
+  },
+  {
+    "revision": "7a59fc06d1b8db7c7504",
+    "url": "/Moviesearcher/static/js/main.7a59fc06.chunk.js"
+  },
+  {
+    "revision": "a529d343692e6ff20dd7",
+    "url": "/Moviesearcher/static/js/1.a529d343.chunk.js"
+  },
+  {
+    "revision": "7a59fc06d1b8db7c7504",
+    "url": "/Moviesearcher/static/css/main.7ec8b7cf.chunk.css"
+  },
+  {
+    "revision": "9def900280c7363d8c9a87fa8772973f",
+    "url": "/Moviesearcher/index.html"
+  }
+];
