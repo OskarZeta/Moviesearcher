0b2b50fe727ba62a15b25e4105f3e30689df029d
Li Dian <dw4h@yandex.ru>
2019-02-16 11:23:08 +0300

    Application deployment

---
+++ docs/precache-manifest.0f0e49293b0f6485a164f792ef3700cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad4b39b2b0139931b84",
+    "url": "/Moviesearcher/static/js/runtime~main.fad4b39b.js"
+  },
+  {
+    "revision": "70b528fd76611599bc09",
+    "url": "/Moviesearcher/static/js/main.70b528fd.chunk.js"
+  },
+  {
+    "revision": "484e58d63f6d1722b9cb",
+    "url": "/Moviesearcher/static/js/1.484e58d6.chunk.js"
+  },
+  {
+    "revision": "70b528fd76611599bc09",
+    "url": "/Moviesearcher/static/css/main.83854490.chunk.css"
+  },
+  {
+    "revision": "ed38bfafaf4b94aa25a9a6c2578ec1cb",
+    "url": "/Moviesearcher/index.html"
+  }
+];
